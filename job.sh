3d55dac7e4bcceec6dc52a5eca28c82896e371eb
Raphael Saldanha <rfsaldanha@gmail.com>
2024-12-05 17:48:46 +0100

    Add folder

--- job.sh
+++ job.sh
@@ -6,6 +6,6 @@ for i in $(seq -f '%02g' 01 20);
 do curl -O --output-dir alertarioradar/temp --url "${base_url}$i.png"; 
 done
 
-convert -delay 20 -loop 0 temp/*.png alertario.gif
+convert -delay 20 -loop 0 alertarioradar/temp/*.png alertario.gif
 
 cp alertarioradar/alertario.gif /dados/htdocs/shiny.icict.fiocruz.br/
