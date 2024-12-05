efa3fc0804b8230e8925b131e0627f952ac8e4e9
Raphael Saldanha <rfsaldanha@gmail.com>
2024-12-05 17:43:50 +0100

    folder change

--- job.sh
+++ job.sh
@@ -3,9 +3,9 @@
 base_url="http://alertario.rio.rj.gov.br/upload/Mapa/comfundo/radar0"
 
 for i in $(seq -f '%02g' 01 20); 
-do curl -O --output-dir temp --url "${base_url}$i.png"; 
+do curl -O --output-dir alertarioradar/temp --url "${base_url}$i.png"; 
 done
 
 magick -delay 20 -loop 0 temp/*.png alertario.gif
 
-mv alertario.gif /dados/htdocs/shiny.icict.fiocruz.br/
+mv alertarioradar/alertario.gif /dados/htdocs/shiny.icict.fiocruz.br/
